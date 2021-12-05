6fe818327940cd9265ba71c7fa2b8dead5c1428a
Alexander Malyshev <agerasx@gmail.com>
2021-12-05 14:00:51 +0500

    Setting up the first test on PageObject

---
+++ pages/main_page.py
@@ -0,0 +1,13 @@
+import pytest
+import time
+import math
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from .base_page import BasePage
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
